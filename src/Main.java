fe27e1239465338dd940f480b9753aa5a837a2c7
crazy <crazy303@mail.ru>
2023-02-07 21:19:25 +0500

    сделано дз 07.02

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import transport.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,21 +13,42 @@ public class Main {
         auto.add("truck");
         System.out.println(auto);
 
+        DriverB driverB = null;
+        DriverC driverC = null;
+        DriverD driverD = null;
         for (int i = 1; i <= 4; i++) {
-            DriverB driverB = new DriverB("Driver cat.B "+i, true, 5+i);
-            Car car = new Car("Lada " +i, "Granta" +i, 1.7, driverB, Type.VAN);
-            DriverC driverC = new DriverC("Driver cat.C "+i, true, 5+i);
-            Truck truck = new Truck("Brand" +i, "Model" +i, 3.4, driverC, LoadCapacity.getValue(15));
-            DriverD driverD = new DriverD("Driver cat.D "+i, true, 5+i);
-            Bus bus = new Bus("Brand" +i, "Model" +i, 2.6, driverD, Size.getValue(52));
+            driverB = new DriverB("Driver cat.B " + i, true, 5 + i);
+            Car car = new Car("Lada " + i, "Granta" + i, 1.7, driverB, Type.VAN);
+            driverC = new DriverC("Driver cat.C " + i, true, 5 + i);
+            Truck truck = new Truck("Brand" + i, "Model" + i, 3.4, driverC, LoadCapacity.getValue(15));
+            driverD = new DriverD("Driver cat.D " + i, true, 5 + i);
+            Bus bus = new Bus("Brand" + i, "Model" + i, 2.6, driverD, Size.getValue(52));
             printInfo(car);
             printInfo(truck);
             printInfo(bus);
-            Truck.isDiagnosticPassed();
-            Car.isDiagnosticPassed();
-            bus.passDiagnostic();
+ //           Truck.isDiagnosticPassed();
+//            Car.isDiagnosticPassed();
+//            bus.passDiagnostic();
         }
+        Car car = new Car("Lada ", "Granta", 1.7, driverB, Type.VAN);
+        Truck truck = new Truck("Brand", "Model", 3.4, driverC, LoadCapacity.getValue(15));
+        Bus bus = new Bus("Brand", "Model", 2.6, driverD, Size.getValue(52));
+        Mechanic mechanic1 = new Mechanic("name1", "comp1");
+        Mechanic mechanic2 = new Mechanic("name2", "comp2");
+        Mechanic mechanic3 = new Mechanic("name3", "comp3");
 
+        List<Mechanic> mechanics = new ArrayList<>();
+        mechanics.add(mechanic1);
+        mechanics.add(mechanic2);
+        mechanics.add(mechanic3);
+
+        Map<Transport, Mechanic> map = new HashMap<>();
+        map.put(car, mechanic1);
+        map.put(bus, mechanic2);
+        map.put(truck, mechanic3);
+        for (Map.Entry<Transport, Mechanic> item : map.entrySet()) {
+            System.out.println("Transport: " + item.getKey() + " Mechanic: " + item.getValue());
+        }
     }
     private static void printInfo(Transport<?> transport){
         System.out.println("водитель "+transport.getDriver().getName()+" управляет автомобилем "+transport.getBrand()+" и будет участвовать в заезде");
